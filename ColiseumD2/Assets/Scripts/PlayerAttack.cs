18bec9a2c359809be2a61095d7a435062b4159b1
Dmitry Tereshchenko <dmitry.tereshchenko@epita.fr>
2020-05-19 15:36:36 +0200

    coliseum

---
+++ ColiseumD2/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Coliseum
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+        private PhotonView photonView;
+        
+        public GameObject Hand1;
+        public GameObject Hand2;
+        public Weapon myWeapon;
+        public Animator anim;
+        private playerMove EnemyKb;
+        private float Timer;
+        private int c;
+        private PlayerHealth pH;
+
+        void Start()
+        {
+            photonView = GetComponent<PhotonView>();
+            myWeapon = Hand1.GetComponentInChildren<Weapon>();
+            anim = GetComponent<Animator>();
+            pH = GetComponent<PlayerHealth>();
+        }
+
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                Timer += Time.deltaTime;
+                Debug.DrawRay(Hand1.transform.position, transform.forward * myWeapon.attackRange);
+                Debug.DrawRay(Hand2.transform.position, transform.forward * myWeapon.attackRange);
+                if (Input.GetMouseButtonDown(0) && Timer > myWeapon.cooldown && pH.shield)
+                {
+                    Timer = 0f;
+                    DoAttack();
+                    c++;
+                }
+
+                anim.SetBool("leftClick", Input.GetMouseButton(0));
+            }
+        }
+
+        private void DoAttack()
+        {
+            float damage = myWeapon.attackDamage;
+
+            if (c > 2)
+            {
+                damage *= 1.5f;
+                c = 0;
+            }
+
+            /*
+            Ray ray1 = new Ray(Hand1.transform.position, transform.forward);
+            Ray ray2 = new Ray(Hand2.transform.position, transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray1, out hit, myWeapon.attackRange) || Physics.Raycast(ray2, out hit, myWeapon.attackRange))
+            {
+                if(hit.collider.CompareTag("Player"))
+                {
+                    PlayerHealth eHealth = hit.collider.GetComponent<PlayerHealth>();
+                    
+                    
+                    EnemyKb = hit.collider.GetComponent<playerMove>();
+                    Vector3 knockback = Hand1.transform.forward * myWeapon.knockback;
+                    knockback.y = 0;
+                    
+                    hit.transform.GetComponent().RPC ("Damage", damage);
+                    
+                    if (!eHealth.shield)
+                    {
+                        eHealth.TakeDamage(damage);
+                        EnemyKb.knockback = knockback;
+                    }
+                }
+            }*/
+            
+            Ray ray1 = new Ray(Hand1.transform.position, transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray1, out hit, myWeapon.attackRange))
+            {
+                if(hit.collider.CompareTag("Player"))
+                {
+                    RpcTarget target = hit.collider.GetComponent<RpcTarget>();
+                    PhotonView enemyView = hit.transform.GetComponent<PhotonView>();
+                    PlayerHealth enemyHealth = hit.transform.GetComponent<PlayerHealth>();
+                    if (enemyView)
+                    {
+                        /*if (enemyHealth.bonus)
+                        {
+                            enemyView.RPC("Effect", target);
+                        }*/
+                        enemyView.RPC("Damage", target, damage);
+                        //if(!enemyHealth.bonus)
+                        enemyView.RPC("Knockback", target,(Hand1.transform.forward * myWeapon.knockback));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ColiseumD2/Assets/Scripts/Weapon.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coliseum
+{
+
+    public class Weapon : MonoBehaviour
+    {
+        public float attackDamage;
+        public float attackRange;
+        public float cooldown;
+        public string weapon;
+        public float knockback;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            AssignStat();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void AssignStat()
+        {
+            switch (weapon)
+            {
+                case "marteau":
+                    attackDamage = 12;
+                    attackRange = 3;
+                    cooldown = 15;
+                    knockback = 10;
+                    break;
+
+                case "lance":
+                    attackDamage = 6;
+                    attackRange = 6;
+                    cooldown = 5;
+                    knockback = 7.5f;
+                    break;
+
+                case "lame":
+                    attackDamage = 3;
+                    attackRange = 1;
+                    cooldown = 2;
+                    knockback = 5;
+                    break;
+
+                case "claymore":
+                    attackDamage = 10;
+                    attackRange = 4;
+                    cooldown = 8;
+                    knockback = 10;
+                    break;
+
+                case "":
+                    attackDamage = 1;
+                    attackRange = 1;
+                    cooldown = 1;
+                    knockback = 3;
+                    break;
+            }
+        }
+    }
+}
